1774c67d212146d80da05d2904e6ebd893eebe28
Jackelin <jackelinvasquezguzman@gmail.com>
2025-08-04 01:17:38 -0600

    Funcion Agregar estudiantes

--- Actividad_no.13.py
+++ Actividad_no.13.py
@@ -1,6 +1,19 @@
+estudiante={}
+
 #SISTEMA DE GESTIÓN ACADEMICA
 def menu_principal():
     print("---MENÚ---")
     print("1.Agregar estudiante.\n2.Agregar curso con nota.\n3.Consultar Estudiante.")
     print("4.Calcular promedio de estudiante\n5.Verificar si aprueba.\n6.Mostrar todos los estudiantes.\nSalir del programa")
 
+#Función para agregar estudiante
+def agregar_estudiante():
+    id=input("Ingrese ID de estudiante:")
+    nombre=input("Ingrese nombre de estudiante:")
+    carrera=input("Ingrese carrera o programa academico:")
+
+    estudiante[id]={
+        "nombre":nombre,
+        "carrera":carrera,
+        "cursos":{}
+    }
